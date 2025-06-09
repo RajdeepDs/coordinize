cc4553e2903a7c4d9919b0d70dcd121dca2aafc7
RajdeepDs <rajdeepds626@gmail.com>
2025-06-09 12:34:02 +0000

    chore: update dependencies across packages

--- apps/web/src/queries/index.ts
+++ apps/web/src/queries/index.ts
@@ -1,18 +1,7 @@
 "use server";
 
 import { database } from "@coordinize/database/db";
-import { unstable_cache } from "next/cache";
-import { cache } from "react";
 
-export const getWaitlistCount = cache(async () => {
-  return unstable_cache(
-    async () => {
-      return await database.earlyAccess.count();
-    },
-    ["waitlist-count"],
-    {
-      tags: ["waitlist"],
-      revalidate: 60,
-    },
-  )();
-});
+export const getWaitlistCount = async () => {
+  return await database.earlyAccess.count();
+};
