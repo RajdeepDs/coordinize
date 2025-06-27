760b1590e31af845bff146fe61e3bb3fd8850773
RajdeepDs <rajdeepds626@gmail.com>
2025-06-27 07:59:36 +0000

    chore: remove example environment files and update biome configuration

--- apps/web/src/queries/index.ts
+++ apps/web/src/queries/index.ts
@@ -5,7 +5,7 @@ import { unstable_cache } from 'next/cache';
 import { cache } from 'react';
 
 export const getWaitlistCount = cache(async () => {
-  return unstable_cache(
+  return await unstable_cache(
     async () => {
       return await database.earlyAccess.count();
     },
