a4f827710652aa276941c9363b7e831ac91e1500
RajdeepDs <rajdeepds626@gmail.com>
2025-06-29 06:07:33 +0000

    feat: add shortcuts to space picker

--- apps/app/src/components/features/post-composer/post-composer-spaces-picker.tsx
+++ apps/app/src/components/features/post-composer/post-composer-spaces-picker.tsx
@@ -11,11 +11,13 @@ import {
   CommandList,
   CommandSeparator,
 } from '@coordinize/ui/components/command';
+import { LayeredHotkeys } from '@coordinize/ui/components/layered-hotkeys';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '@coordinize/ui/components/popover';
+import { Tooltip } from '@coordinize/ui/components/tooltip';
 import { Icons } from '@coordinize/ui/lib/icons';
 import Link from 'next/link';
 import { useState } from 'react';
@@ -43,76 +45,91 @@ export function PostComposerSpacesPicker({
     );
   }
   return (
-    <Popover onOpenChange={setOpen} open={open}>
-      <PopoverTrigger asChild className="cursor-pointer outline-none">
-        <div className="flex w-fit items-start gap-2 text-sm">
-          <p>
-            {spaces.find((space) => space.identifier === value)?.name ??
-              'Select space'}
-          </p>
-          <span className="flex items-center gap-1 text-muted-foreground">
-            {value || '--'}
-            <Icons.chevronDown size={16} />
-          </span>
-        </div>
-      </PopoverTrigger>
-      <PopoverContent align="start" className="w-full p-0 shadow-md/5">
-        <Command>
-          <CommandInput
-            className="h-8"
-            containerClasses="m-1 px-2 bg-input rounded-sm border"
-            placeholder="Search space..."
-            showIcon={false}
-          />
-          <CommandList>
-            <CommandEmpty className="py-2 text-center text-sm">
-              No space found.
-            </CommandEmpty>
-            <CommandGroup className="p-1">
-              {spaces.map((space) => (
-                <CommandItem
-                  className="cursor-pointer"
-                  key={space.identifier}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue);
-                    setOpen(false);
-                  }}
-                  value={space.identifier}
+    <>
+      <LayeredHotkeys
+        callback={() => setOpen(true)}
+        keys={'ctrl+shift+m'}
+        options={{ preventDefault: true }}
+      />
+
+      <Popover onOpenChange={setOpen} open={open}>
+        <Tooltip
+          align="start"
+          label="Set space"
+          shortcut="ctrl+shift+m"
+          side="bottom"
+        >
+          <PopoverTrigger asChild className="cursor-pointer outline-none">
+            <div className="flex w-fit items-start gap-2 text-sm">
+              <p>
+                {spaces.find((space) => space.identifier === value)?.name ??
+                  'Select space'}
+              </p>
+              <span className="flex items-center gap-1 text-muted-foreground">
+                {value || '--'}
+                <Icons.chevronDown size={16} />
+              </span>
+            </div>
+          </PopoverTrigger>
+        </Tooltip>
+        <PopoverContent align="start" className="w-full p-0 shadow-md/5">
+          <Command>
+            <CommandInput
+              className="h-7"
+              containerClasses="m-1 px-2 bg-input rounded-sm border"
+              placeholder="Search space..."
+              showIcon={false}
+            />
+            <CommandList>
+              <CommandEmpty className="py-2 text-center text-sm">
+                No space found.
+              </CommandEmpty>
+              <CommandGroup className="p-1">
+                {spaces.map((space) => (
+                  <CommandItem
+                    className="cursor-pointer"
+                    key={space.identifier}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue);
+                      setOpen(false);
+                    }}
+                    value={space.identifier}
+                  >
+                    <Icons.space className="text-muted-foreground" size={16} />
+                    {space.name}
+                    {value === space.identifier && (
+                      <Icons.check
+                        className="ml-auto text-muted-foreground"
+                        size={16}
+                      />
+                    )}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+              <CommandSeparator />
+              <CommandGroup className="p-1">
+                <Button
+                  asChild
+                  className="w-full justify-start gap-3 px-2 font-normal text-accent-foreground hover:bg-ui-gray-100"
+                  size={'sm'}
+                  tooltip="Create a new space"
+                  tooltipSide="right"
+                  variant="ghost"
                 >
-                  <Icons.space className="text-muted-foreground" size={16} />
-                  {space.name}
-                  {value === space.identifier && (
-                    <Icons.check
-                      className="ml-auto text-muted-foreground"
+                  <Link href={`/${workspaceSlug}/settings/new-space`}>
+                    <Icons.plus
+                      aria-hidden="true"
+                      className="text-muted-foreground"
                       size={16}
                     />
-                  )}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-            <CommandSeparator />
-            <CommandGroup className="p-1">
-              <Button
-                asChild
-                className="w-full justify-start gap-3 px-2 font-normal text-accent-foreground hover:bg-ui-gray-100"
-                size={'sm'}
-                tooltip="Create a new space"
-                tooltipSide="right"
-                variant="ghost"
-              >
-                <Link href={`/${workspaceSlug}/settings/new-space`}>
-                  <Icons.plus
-                    aria-hidden="true"
-                    className="text-muted-foreground"
-                    size={16}
-                  />
-                  New space
-                </Link>
-              </Button>
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+                    New space
+                  </Link>
+                </Button>
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
