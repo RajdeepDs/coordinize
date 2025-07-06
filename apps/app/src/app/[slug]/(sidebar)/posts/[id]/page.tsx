05678d7b14d1ec50e6aba1c6cf2f1d904376a0d6
RajdeepDs <rajdeepds626@gmail.com>
2025-07-06 17:11:27 +0000

    feat: add PostMetadata component and integrate it into PostPage for displaying post details

--- apps/app/src/app/[slug]/(sidebar)/posts/[id]/page.tsx
+++ apps/app/src/app/[slug]/(sidebar)/posts/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { ActivitySection } from '@/components/features/activity/activity-section';
 import { PostHeader } from '@/components/layout/post/post-header';
+import { PostMetadata } from '@/components/layout/post/post-metadata';
 import { getQueryClient, trpc } from '@/trpc/server';
 
 export default async function PostPage({
@@ -14,19 +15,25 @@ export default async function PostPage({
     trpc.post.getPostById.queryOptions({ id })
   );
 
-  if (!post) {
-    return (
-      <div className="flex h-full w-full items-center justify-center">
-        Post not found.
-      </div>
-    );
-  }
-
   return (
     <div className="flex h-full w-full gap-1.5 overflow-hidden">
       <div className="flex-1 rounded border bg-background">
-        <PostHeader space={post.space.name} title={post.title} />
-        <ActivitySection>Hello</ActivitySection>
+        {post ? (
+          <>
+            <PostHeader space={post.space.name} title={post.title} />
+            <ActivitySection>
+              <PostMetadata
+                createdAt={post.createdAt}
+                userImage={post.author.image || ''}
+                userName={post.author.name}
+              />
+            </ActivitySection>
+          </>
+        ) : (
+          <div className="flex h-full items-center justify-center">
+            <p className="text-ui-gray-900">Post not found.</p>
+          </div>
+        )}
       </div>
     </div>
   );
